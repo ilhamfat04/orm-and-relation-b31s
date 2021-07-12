a55ea0b17d503072b9caa7be704f0dc9ac5ba8f4
Jody Septiawan <jody@192.168.1.2>
2021-07-12 09:43:48 +0700

    Delete data with ORM

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -93,3 +93,26 @@ exports.updateUser = async (req, res) => {
         })
     }
 }
+
+exports.deleteUser = async (req, res) => {
+    try {
+        const { id } = req.params
+
+        await user.destroy({
+            where: {
+                id
+            }
+        })
+
+        res.send({
+            status: 'success',
+            message: `Delete user id: ${id} finished`
+        })
+    } catch (error) {
+        console.log(error)
+        res.send({
+            status: 'failed',
+            message: 'Server Error'
+        })
+    }
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,13 @@ const express = require('express')
 const router = express.Router()
 
 // Controller
-const { addUsers, getUsers, getUser, updateUser } = require('../controllers/user')
+const { addUsers, getUsers, getUser, updateUser, deleteUser } = require('../controllers/user')
 
 // Route
 router.post('/user', addUsers)
 router.get('/users', getUsers)
 router.get('/user/:id', getUser)
 router.patch('/user/:id', updateUser)
+router.delete('/user/:id', deleteUser)
 
 module.exports = router
