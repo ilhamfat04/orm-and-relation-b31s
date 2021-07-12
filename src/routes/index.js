73e7f99415f7e8b566fbf33e7f71fddbd1aacf72
Jody Septiawan <jody@192.168.1.2>
2021-07-12 09:34:44 +0700

    Update data with ORM

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,12 @@ const express = require('express')
 const router = express.Router()
 
 // Controller
-const { addUsers, getUsers, getUser } = require('../controllers/user')
+const { addUsers, getUsers, getUser, updateUser } = require('../controllers/user')
 
 // Route
 router.post('/user', addUsers)
 router.get('/users', getUsers)
 router.get('/user/:id', getUser)
+router.patch('/user/:id', updateUser)
 
 module.exports = router
