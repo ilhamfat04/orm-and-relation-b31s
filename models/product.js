1a711ec533bfb22f53634a95d6f30d0dd3df9c8c
Jody Septiawan <jody@192.168.1.2>
2021-07-12 11:28:23 +0700

    Prepare database

---
+++ models/product.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class product extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  product.init({
+    name: DataTypes.STRING,
+    desc: DataTypes.TEXT,
+    price: DataTypes.INTEGER,
+    qty: DataTypes.INTEGER,
+    idUser: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'product',
+  });
+  return product;
+};
