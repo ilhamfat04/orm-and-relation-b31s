147ffcf454f41c5b3e6a78d240526db6751e18ef
lenovo <yosepalexsander9@gmail.com>
2021-07-14 17:37:12 +0700

    done

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -95,24 +95,5 @@ exports.updateUser = async (req, res) => {
 }
 
 exports.deleteUser = async (req, res) => {
-    try {
-        const { id } = req.params
-
-        await user.destroy({
-            where: {
-                id
-            }
-        })
-
-        res.send({
-            status: 'success',
-            message: `Delete user id: ${id} finished`
-        })
-    } catch (error) {
-        console.log(error)
-        res.send({
-            status: 'failed',
-            message: 'Server Error'
-        })
-    }
+    // code here
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,13 +3,14 @@ const express = require('express')
 const router = express.Router()
 
 // Controller
-const { addUsers, getUsers, getUser, updateUser, deleteUser } = require('../controllers/user')
+// import controller function here 
+const { addUsers, getUsers, getUser, updateUser } = require('../controllers/user')
 
 // Route
 router.post('/user', addUsers)
 router.get('/users', getUsers)
 router.get('/user/:id', getUser)
 router.patch('/user/:id', updateUser)
-router.delete('/user/:id', deleteUser)
+// add route here
 
 module.exports = router
