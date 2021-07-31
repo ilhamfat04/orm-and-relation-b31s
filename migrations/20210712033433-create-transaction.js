98e99ca6efa775e83830d22f480c6ca4ce8f5d77
yosep <yosepalexsander9@gmail.com>
2021-07-31 19:30:25 +0700

    update

--- migrations/20210712033433-create-transaction.js
+++ migrations/20210712033433-create-transaction.js
@@ -38,6 +38,9 @@ module.exports = {
       price: {
         type: Sequelize.INTEGER
       },
+      status: {
+        type: Sequelize.STRING
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
