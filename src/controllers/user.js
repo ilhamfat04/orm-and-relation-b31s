2b4cb1d8b612f5ee7bcd1ea71901f2f4e02f612e
= <=>
2022-03-09 13:43:26 +0700

    update

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,5 +1,21 @@
 // import model here
+const { user } = require('../../models')
 
-exports.addUsers = async (req, res) => {
+exports.addUser = async (req, res) => {
     // code here
+    try {
+        const data = req.body
+        const createdData = await user.create(data)
+
+        res.send({
+            status: "success",
+            data: createdData
+        })
+    } catch (error) {
+        console.log(error);
+        res.send({
+            status: "failed",
+            message: "Server error"
+        })
+    }
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router()
 
 // Controller
 // import controller here
+const { addUser } = require('../controllers/user')
 
 // Route
 // add route here
+router.post('/user', addUser)
 
 
 module.exports = router
